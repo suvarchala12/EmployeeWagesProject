d4399c1c90a885c2f5e6f9869af994ab253ea503
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-27 15:31:28 +0530

    uc1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("EmployeeWagesProject");
+﻿using System;
+// check employee pesent or not.
+class Program
+{
+    public void empChek()
+    {
+        //constants
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present = " + empCheck);
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent = " + empCheck);
+        }
+    }
+    static void Main(string[] args)
+    {
+        Program emp = new Program();
+
+        emp.empChek();
+    }
+}
