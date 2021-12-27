2e884d54168bdcc8c19af0914751f3603df7ad36
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-27 15:45:21 +0530

    uc5

--- Program.cs
+++ Program.cs
@@ -1,44 +1,46 @@
 ﻿using System;
-// check employee Wage on part time or full time using switch case.
+// check employee Wage on part time or full time monthly.
 class Program
 {
-    public void empswitch()
+    public void mothlyWage()
     {
         //constants
         const int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         const int IS_PART_TIME = 2;
+        const int NUM_OF_WORKING_DAYS = 20;
 
         // Variables
         int emphrs = 0;
         int empwage = 0;
+        int totalSalary = 0;
 
-        Random random = new Random();
-
-        int empCheck = random.Next(0, 3);
-
-        switch (empCheck)
+        for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
         {
-            case IS_FULL_TIME:
-                Console.WriteLine("Employee is full time");
-                emphrs = 8;
-                break;
-            case IS_PART_TIME:
-                Console.WriteLine("Employee is part time");
-                emphrs = 4;
-                break;
-            default:
-                Console.WriteLine("Employee is absent");
-                emphrs = 0;
-                break;
-        }
+            Random random = new Random();
 
-        empwage = emphrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("emp Wage: " + empwage);
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    emphrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    emphrs = 4;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            totalSalary = totalSalary + empwage;
+        }
+        Console.WriteLine(" monthly salry is " + totalSalary);
     }
     static void Main(string[] args)
     {
-        Program empSwitch = new Program();
-        empSwitch.empswitch();
+        Program monthly = new Program();
+        monthly.mothlyWage();
     }
 }
