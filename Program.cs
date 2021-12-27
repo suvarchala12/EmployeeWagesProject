b13b5f9948703a44ef82e2796a2d1923b2fb08d7
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-27 15:47:38 +0530

    uc6

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-// check employee Wage on part time or full time monthly.
+// Calculate Wages till a condition of total working hours or
+// days is reached for a month
 class Program
 {
-    public void mothlyWage()
+    public void empWage()
     {
         //constants
         const int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         const int IS_PART_TIME = 2;
         const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
 
         // Variables
         int emphrs = 0;
-        int empwage = 0;
-        int totalSalary = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
@@ -33,14 +36,15 @@ class Program
                     emphrs = 0;
                     break;
             }
-            empwage = emphrs * EMP_RATE_PER_HOUR;
-            totalSalary = totalSalary + empwage;
+            totalEmpHrs = totalEmpHrs + emphrs;
+            Console.WriteLine("Working day " + totalWorkingDays + " Employee hours " + emphrs);
         }
-        Console.WriteLine(" monthly salry is " + totalSalary);
+        int totalSalary = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine(" Total salary is " + totalSalary);
     }
     static void Main(string[] args)
     {
-        Program monthly = new Program();
-        monthly.mothlyWage();
+        Program emp = new Program();
+        emp.empWage();
     }
 }
